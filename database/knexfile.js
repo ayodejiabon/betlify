896fc8873b45151219b4af29f22e49ca779ba232
Abon Ayodeji <abonayodeji@gmail.com>
2021-06-13 21:52:25 +0100

    db_fix

--- database/knexfile.js
+++ database/knexfile.js
@@ -27,6 +27,7 @@ module.exports = {
     client: process.env.DB_DIALECT,
     connection: {
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
@@ -47,6 +48,7 @@ module.exports = {
     client: process.env.DB_DIALECT,
     connection: {
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
