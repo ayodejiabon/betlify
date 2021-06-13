d138e495e580bb3fe0774ab811cb119326ae49e2
Abon Ayodeji <abonayodeji@gmail.com>
2021-06-13 23:21:55 +0100

    ci_deploy_2

--- controllers/planController.js
+++ controllers/planController.js
@@ -5,15 +5,14 @@ const error = require('../utils/appError');
 exports.getPlans = catchAsync ( async (req, res, next) => {
 
   	try {
-
   		const plans = await pool.select('plan_id', 'name', 'description', 'daily_interest', 'duration', 'min_deposit', 'max_deposit', 'percentage').from('invest_plans').where('status', '=', '1');
 
   		return res.status(200).json({
 	        status:"success",
 	        data:plans
 	    });
-
   	} catch (err) {
+  		console.log(err);
 
   		return res.status(500).json({
 	        status: "failed",
